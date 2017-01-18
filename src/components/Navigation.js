65a4b6cf182c1efd6e012b8e58c473c55a91fd5e
Ivan Morales <ivan@imorales.com>
2017-01-18 01:19:30 -0600

    stuff

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router';
 import Headroom from 'react-headroom';
 
 import {
+  Container,
+
   Nav,
   NavGroup,
   NavItem,
@@ -25,32 +27,31 @@ class Navigation extends Component {
     });
   }
   render() {
-    const routes = ['Our Work', 'Our Process', 'Our Crew', 'Contact'];
+    const routes = ['Work', 'Process', 'Crew', 'Connect'];
     const toggle = this.state.toggle ? '__re-bulma_is-active' : '';
 
     return (
       <Headroom>
-        <Nav className="ssl--navigation">
-          <NavGroup align="left">
-            <NavItem isBrand>
-              <Link to=""><img src={logo} className="ssl--nav-logo" alt="logo" /></Link>
-            </NavItem>
-          </NavGroup>
-          <NavToggle onClick={this.toggleNavigation.bind(this)}/>
-          <NavGroup align="right" isMenu id="nav-menu" className={`ssl--nav-group ${toggle}`}>
-            {routes.map( (route, i) => {
-              let routeTo = route.toLowerCase().replace(/\W+/,'-');
-              if (route === 'Home') {
-                routeTo = '';
-              }
-              return (
-                <NavItem key={i} onClick={this.toggleNavigation.bind(this)}>
-                  <Link to={routeTo} className="ssl--nav-item">{route}</Link>
-                </NavItem>
-              );
-            })}
-          </NavGroup>
-        </Nav>
+        <Container>
+          <Nav className="ssl--navigation">
+            <NavGroup align="left">
+              <NavItem isBrand>
+                <Link to=""><img src={logo} className="ssl--nav-logo" alt="logo" /></Link>
+              </NavItem>
+            </NavGroup>
+            <NavToggle onClick={this.toggleNavigation.bind(this)}/>
+            <NavGroup align="right" isMenu id="nav-menu" className={`ssl--nav-group ${toggle}`}>
+              {routes.map( (route, i) => {
+                let routeTo = route.toLowerCase().replace(/\W+/,'-');
+                return (
+                  <NavItem key={i} onClick={this.toggleNavigation.bind(this)}>
+                    <Link to={routeTo} className={'ssl--nav-item ssl--nav-item-' + routeTo}>{route.toUpperCase()}</Link>
+                  </NavItem>
+                );
+              })}
+            </NavGroup>
+          </Nav>
+        </Container>
       </Headroom>
     );
   }
